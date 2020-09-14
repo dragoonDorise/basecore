0a8df6714a7ef329cbf63143fa660482ddc523d4
Rodrigo <rsedano@deloittedigital.es>
2020-09-14 17:16:18 +0200

    NPM Fixes

--- src/app/app.js
+++ src/app/app.js
@@ -2,43 +2,43 @@
 //  Utils
 //
 
-import { buttonTextChange } from "../../node_modules/basecore/src/components/_utils-js/_buttonTextChange/_buttonTextChange.js";
+import { buttonTextChange } from "../../node_modules/getbasecore/src/components/_utils-js/_buttonTextChange/_buttonTextChange.js";
 buttonTextChange();
 
-import { buttonDismis } from "../../node_modules/basecore/src/components/_utils-js/_buttonDismis/_buttonDismis.js";
+import { buttonDismis } from "../../node_modules/getbasecore/src/components/_utils-js/_buttonDismis/_buttonDismis.js";
 buttonDismis();
 
-import { collapse } from "../../node_modules/basecore/src/components/_utils-js/_collapse/_collapse.js";
+import { collapse } from "../../node_modules/getbasecore/src/components/_utils-js/_collapse/_collapse.js";
 collapse();
 
-import { inputNumbersButtons } from "../../node_modules/basecore/src/components/_utils-js/_inputNumbersButtons/_inputNumbersButtons.js";
+import { inputNumbersButtons } from "../../node_modules/getbasecore/src/components/_utils-js/_inputNumbersButtons/_inputNumbersButtons.js";
 inputNumbersButtons();
 
-import { lazyload } from "../../node_modules/basecore/src/components/_utils-js/_lazyload/_lazyload.js";
+import { lazyload } from "../../node_modules/getbasecore/src/components/_utils-js/_lazyload/_lazyload.js";
 lazyload();
 
-import { noNumber } from "../../node_modules/basecore/src/components/_utils-js/_noNumber/_noNumber.js";
+import { noNumber } from "../../node_modules/getbasecore/src/components/_utils-js/_noNumber/_noNumber.js";
 noNumber();
 
-import { offCanvas } from "../../node_modules/basecore/src/components/_utils-js/_off-canvas/_off-canvas.js";
+import { offCanvas } from "../../node_modules/getbasecore/src/components/_utils-js/_off-canvas/_off-canvas.js";
 offCanvas();
 
-import { removeFocus } from "../../node_modules/basecore/src/components/_utils-js/_removeFocus/_removeFocus.js";
+import { removeFocus } from "../../node_modules/getbasecore/src/components/_utils-js/_removeFocus/_removeFocus.js";
 removeFocus();
 
-import { revealPassword } from "../../node_modules/basecore/src/components/_utils-js/_revealPassword/_revealPassword.js";
+import { revealPassword } from "../../node_modules/getbasecore/src/components/_utils-js/_revealPassword/_revealPassword.js";
 revealPassword();
 
-import { scrollTo } from "../../node_modules/basecore/src/components/_utils-js/_scrollTo/_scrollTo.js";
+import { scrollTo } from "../../node_modules/getbasecore/src/components/_utils-js/_scrollTo/_scrollTo.js";
 scrollTo();
 
-import { siemaExtend } from "../../node_modules/basecore/src/components/_utils-js/_siemaExtend/_siemaExtend.js";
+import { siemaExtend } from "../../node_modules/getbasecore/src/components/_utils-js/_siemaExtend/_siemaExtend.js";
 siemaExtend();
 
-import { textareaAutosize } from "../../node_modules/basecore/src/components/_utils-js/_textareaAutosize/_textareaAutosize.js";
+import { textareaAutosize } from "../../node_modules/getbasecore/src/components/_utils-js/_textareaAutosize/_textareaAutosize.js";
 textareaAutosize();
 
-import { toggle } from "../../node_modules/basecore/src/components/_utils-js/_toggle/_toggle.js";
+import { toggle } from "../../node_modules/getbasecore/src/components/_utils-js/_toggle/_toggle.js";
 toggle();
 //
 // //
@@ -46,13 +46,13 @@ toggle();
 // //
 //
 
-import { modal } from "../../node_modules/basecore/src/components/_molecules/_modal/_modal.js";
+import { modal } from "../../node_modules/getbasecore/src/components/_molecules/_modal/_modal.js";
 modal();
 
-import { dropdown } from "../../node_modules/basecore/src/components/_molecules/_dropdown/_dropdown.js";
+import { dropdown } from "../../node_modules/getbasecore/src/components/_molecules/_dropdown/_dropdown.js";
 dropdown();
 
-import { tabs } from "../../node_modules/basecore/src/components/_molecules/_tabs/_tabs.js";
+import { tabs } from "../../node_modules/getbasecore/src/components/_molecules/_tabs/_tabs.js";
 tabs();
 
 //
