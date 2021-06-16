b13fce96ee5110a2a3412f77feec94f6fc40b8d7
R <>
2021-06-16 16:27:47 +0200

    More cleaning...

--- src/components/atoms/Iframe/Iframe.js
+++ src/components/atoms/Iframe/Iframe.js
@@ -3,14 +3,14 @@ import { PropTypes } from "prop-types";
 import "./iframe.scss";
 export const Iframe = ({ src, css }) => {
   return (
-    <div class="embed-responsive embed-responsive--16-9">
+    <div className="embed-responsive embed-responsive--16-9">
       <iframe
-        class="embed-responsive__item"
+        className="embed-responsive__item"
         width="1280"
         height="720"
         src={src}
-        frameborder="0"
-        allowfullscreen
+        frameBorder="0"
+        allowFullScreen
       ></iframe>
     </div>
   );
--- src/hooks/useConfig.js
+++
@@ -1,9 +0,0 @@
-export const useConfig = () => {
-  let wsValues = {};
-  if (window.location.href.indexOf("test") !== -1) {
-    wsValues = {};
-  } else {
-    wsValues = {};
-  }
-  return wsValues;
-};
