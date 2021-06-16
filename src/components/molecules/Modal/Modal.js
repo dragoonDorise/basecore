f92312648ef21984e17f5448f10724bfbc6458aa
R <>
2021-06-16 11:56:47 +0200

    Empty molecules todo

---
+++ src/components/molecules/Modal/Modal.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+export const Modal = ({ propa }) => {
+  const [state, setState] = useState("{active:false}");
+
+  return <>Modal</>;
+};
---
+++ src/components/molecules/Tooltip/Tooltip.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+export const Tooltip = ({ propa }) => {
+  const [state, setState] = useState("{active:false}");
+
+  return <>Modal</>;
+};
