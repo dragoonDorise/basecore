d108080f6b608ffb2ea6a1c1e503055822d4debe
R <>
2021-06-16 17:41:31 +0200

    Global components exports

---
+++ Atoms.js
@@ -0,0 +1,15 @@
+
+export  {BtnSimple} from "./src/components/atoms/BtnSimple/BtnSimple";
+export  {BtnSwitch} from "./src/components/atoms/BtnSwitch/BtnSwitch";
+export  {Icon} from "./src/components/atoms/Icon/Icon";
+export  {LinkSimple} from "./src/components/atoms/LinkSimple/LinkSimple";
+export  {Img} from "./src/components/atoms/Img/Img";
+export  {Iframe} from "./src/components/atoms/Iframe/Iframe";
+export  {List} from "./src/components/atoms/List/List";
+export  {LoaderCircle} from "./src/components/atoms/LoaderCircle/LoaderCircle";
+export  {ProgressBar} from "./src/components/atoms/ProgressBar/ProgressBar";
+export  {FormInputSimple} from "./src/components/atoms/FormInputSimple/FormInputSimple";
+export  {FormSelectSimple} from "./src/components/atoms/FormInputSimple/FormSelectSimple";
+export  {FormRadioSimple} from "./src/components/atoms/FormRadioSimple/FormRadioSimple";
+export  {FormCheckboxSimple} from "./src/components/atoms/FormCheckboxSimple/FormCheckboxSimple";
+export  {FormInputRangeSimple} from "./src/components/atoms/FormInputRangeSimple/FormInputRangeSimple";
---
+++ Molecules.js
@@ -0,0 +1,8 @@
+export  { Pagination} from "./src/components/molecules/Pagination/Pagination";
+export  { Form}  from "./src/components/molecules/Form/Form";
+export  { Alert}  from "./src/components/molecules/Alert/Alert";
+export  { Table}  from "./src/components/molecules/Table/Table";
+export  { Accordion}  from "./src/components/molecules/Accordion/Accordion";
+export  { Breadcrumb}  from "./src/components/molecules/Breadcrumb/Breadcrumb";
+export  { Dropdown}  from "./src/components/molecules/Dropdown/Dropdown";
+export  { Tabs}  from "./src/components/molecules/Tabs/Tabs";
